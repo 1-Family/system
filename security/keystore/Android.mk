2f7d2858d39a7cf95e06d2cd6f850ff0b61d8058
HelixOS <vitaliy@thecto.co.il>
2017-03-01 11:36:45 +0200

    Update Android.mk

--- security/keystore/Android.mk
+++ security/keystore/Android.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2009 The Android Open Source Project
+# Modifications Copyright (C) 2011-2017 TheCTO Technological Entrepreneurship & Consulting LTD
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

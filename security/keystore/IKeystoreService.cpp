5cd2130855bd2edf5fb1e2d0624e124ae6fe40f7
HelixOS <vitaliy@thecto.co.il>
2017-03-01 11:37:39 +0200

    Update IKeystoreService.cpp

--- security/keystore/IKeystoreService.cpp
+++ security/keystore/IKeystoreService.cpp
@@ -1,7 +1,8 @@
 /*
 **
 ** Copyright 2008, The Android Open Source Project
-**
+** Modifications Copyright (C) 2011-2017 TheCTO Technological Entrepreneurship & Consulting LTD
+** 
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
 ** You may obtain a copy of the License at
